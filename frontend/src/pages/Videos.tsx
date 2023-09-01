709afd3b433368f189ba8267790d6bd061f60eda
JulioVianaDev <JulioVianaDev@gmail.com>
2023-09-01 15:32:35 -0300

    falta implementar o delete tirar o video da tela

--- frontend/src/pages/Videos.tsx
+++ frontend/src/pages/Videos.tsx
@@ -2,6 +2,7 @@ import React,{useState,useEffect} from 'react'
 import { videoProps } from '../interfaces/videoProps' 
 import Video from '../components/Video'
 import {getVideosData} from '../hooks/getVideosData'
+import { DELETED_VIDEO } from '../types/deleteVideoTypes'
 function Videos() {
   const [videos,setVideos] = useState<videoProps[]>([])
 
@@ -10,6 +11,12 @@ function Videos() {
       .then((res:videoProps[])=>setVideos(res))
       .catch((err:any)=>console.log(err))
   },[])
+
+  const deleteVideoWithSucccess = (resHook: string,id: string)=>{
+    if (resHook == DELETED_VIDEO){
+      setVideos(videos.filter(v => v._id !== id))
+    }
+  }
   return (
     <div>
        <h1>Videos inscritos</h1>
